3dcb2a3191db2ba6ae7d585f9f8349e9f914f3bd
nanangarsyadw <haoxinanz413@163.com>
2022-02-10 00:24:28 +0800

    修改格式

--- src/com/zftlive/android/data/DTB.java
+++ src/com/zftlive/android/data/DTB.java
@@ -2,31 +2,32 @@ package com.zftlive.android.data;
 
 /**
  * 数据传输Bean（继承DTO）
+ * 
  * @author 曾繁添
  * @version 1.0
  * @param <V>
  * @param <K>
- *
+ * 
  */
 public class DTB<V, K> extends DTO<K, V> {
-	
-		private String name;
-		
-		private String nameCN;
 
-		public String getName() {
-			return name;
-		}
+	private String name;
 
-		public void setName(String name) {
-			this.name = name;
-		}
+	private String nameCN;
 
-		public String getNameCN() {
-			return nameCN;
-		}
+	public String getName() {
+		return name;
+	}
 
-		public void setNameCN(String nameCN) {
-			this.nameCN = nameCN;
-		}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNameCN() {
+		return nameCN;
+	}
+
+	public void setNameCN(String nameCN) {
+		this.nameCN = nameCN;
+	}
 }
