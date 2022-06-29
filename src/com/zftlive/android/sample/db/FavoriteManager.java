205440135f29fba295ddbab6e1c18cf83f3f9b9a
nanangarsyadw <haoxinanz413@163.com>
2022-06-29 08:51:19 +0800

    增加批量增加代码

--- src/com/zftlive/android/sample/db/FavoriteManager.java
+++ src/com/zftlive/android/sample/db/FavoriteManager.java
@@ -2,6 +2,7 @@ package com.zftlive.android.sample.db;
 
 import java.sql.Savepoint;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 import android.content.Context;
 import android.content.ServiceConnection;
@@ -66,8 +67,57 @@ public class FavoriteManager {
 		return rows;
 	}
 
-	public void insertBatch() {
+	public int insertBatch(final List<Favorite> data) {
+		// 返回受影响行数
+		int rows = 0;
+		
+		long start = System.currentTimeMillis();
+		
+		String savePointName = "FavoriteinsertBatch";
+		Savepoint savePoint = null;
+		DatabaseConnection connection = null;
+		try {
+			// 获取数据库连接
+			connection = dbHelper.getConnection();
+			connection.setAutoCommit(false);
+			savePoint = connection.setSavePoint(savePointName);
 
+			// 插入数据
+			final Dao<Favorite, String> dao = dbHelper.getDao(Favorite.class);
+
+			for (Favorite favorite : data) {
+				rows += dao.create(favorite);
+			}
+
+//			rows = dao.callBatchTasks(new Callable<Integer>() {
+//
+//				@Override
+//				public Integer call() throws Exception {
+//					int rows = 0;
+//					for (Favorite favorite : data) {
+//						rows += dao.create(favorite);
+//					}
+//					return rows;
+//				}
+//			});
+
+			// 提交事务
+			connection.commit(savePoint);
+
+		} catch (Exception e) {
+			// 回滚事务
+			dbHelper.rollback(connection, savePoint);
+
+			Log.e(TAG, "插入数据失败，原因：" + e.getMessage());
+		} finally {
+			// 释放数据库连接
+			dbHelper.closeConnection(connection);
+		}
+		
+		long end = System.currentTimeMillis();
+		Log.e(TAG, "操作耗时：" + (start-end)/1000 );
+
+		return rows;
 	}
 
 	public void delete(Favorite model) {
