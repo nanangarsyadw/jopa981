95c822c47e321da150508df655478b189607d3d8
nanangarsyadw <haoxinanz413@163.com>
2022-08-11 12:57:10 +0800

    集成fadingactionbar类库

---
+++ src/com/zftlive/android/view/fadingactionbar/view/ObservableScrollable.java
@@ -0,0 +1,6 @@
+package com.zftlive.android.view.fadingactionbar.view;
+
+
+public interface ObservableScrollable {
+    void setOnScrollChangedCallback(OnScrollChangedCallback callback);
+}
---
+++ src/com/zftlive/android/view/fadingactionbar/view/OnScrollChangedCallback.java
@@ -0,0 +1,5 @@
+package com.zftlive.android.view.fadingactionbar.view;
+
+public interface OnScrollChangedCallback {
+    void onScroll(int l, int t);
+}
