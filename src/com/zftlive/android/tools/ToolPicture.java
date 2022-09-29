e59d04a15b34cba0abbb156cf4901e5809bfcabf
nanangarsyadw <haoxinanz413@163.com>
2022-09-29 11:26:00 +0800

    增加生成含LOGO的二维码图片方法

--- src/com/zftlive/android/tools/ToolPicture.java
+++ src/com/zftlive/android/tools/ToolPicture.java
@@ -2,6 +2,7 @@ package com.zftlive.android.tools;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.Hashtable;
 import java.util.Random;
 
@@ -27,6 +28,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.NinePatchDrawable;
 import android.media.ExifInterface;
+import android.util.Log;
 import android.view.View;
 
 import com.google.zxing.BarcodeFormat;
@@ -34,6 +36,7 @@ import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import com.zftlive.android.config.SysEnv;
 
 /**
@@ -115,8 +118,13 @@ public class ToolPicture {
 		if (!ToolString.isNoBlankAndNoNull(content))
 			return null;
 
-		Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
+		//二维码参数设置
+		Hashtable<EncodeHintType, Object> hints = new Hashtable<EncodeHintType, Object>();
+		//容错率L=7% M=15% Q=25% H=30% 容错率越高，越容易被快速扫描
+		hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
 		hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+		hints.put(EncodeHintType.MARGIN, 1);
+		
 		// 图像数据转换，使用了矩阵转换
 		BitMatrix bitMatrix = new QRCodeWriter().encode(content,
 				BarcodeFormat.QR_CODE, width, height, hints);
@@ -135,6 +143,71 @@ public class ToolPicture {
 				Bitmap.Config.ARGB_8888);
 		bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
 		
+		return bitmap;
+	}
+	
+	/**
+	 * 生成带Logo的二维码
+	 * @param strContent 二维码内容
+	 * @param qrWH 二维码的宽高
+	 * @param mLogoBitmap Logo图片
+	 * @param logoWH LOGO的宽高(注意：LOGO的宽高不能最多只能是生成二维码图片的0.2)
+	 * @return
+	 * @throws WriterException 生成二维码失败异常
+	 */
+	public Bitmap makeQRImageWithLogo(String strContent,int qrWH,Bitmap mLogoBitmap,int logoWH) throws WriterException {
+
+		//缩放Logo图片
+		Matrix logoMatrix = new Matrix();
+		float sx = (float) 2 * logoWH / mLogoBitmap.getWidth();
+		float sy = (float) 2 * logoWH / mLogoBitmap.getHeight();
+		logoMatrix.setScale(sx, sy);
+		mLogoBitmap = Bitmap.createBitmap(mLogoBitmap, 0, 0,mLogoBitmap.getWidth(), mLogoBitmap.getHeight(), logoMatrix,false);
+		
+		//二维码参数设置
+		Hashtable<EncodeHintType, Object> hints = new Hashtable<EncodeHintType, Object>();
+		//容错率L=7% M=15% Q=25% H=30% 容错率越高，越容易被快速扫描
+		hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+		hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+		hints.put(EncodeHintType.MARGIN, 1);
+
+		QRCodeWriter write = new QRCodeWriter();
+		try {
+			Field menuKeyField = QRCodeWriter.class.getDeclaredField("QUIET_ZONE_SIZE");
+			if(menuKeyField != null) {
+			    menuKeyField.setAccessible(true);
+			    menuKeyField.setInt(write,1);//控制的边框的宽度
+			}
+		} catch (Exception e) {
+			Log.e("cretaeBitmap", "反射设置QRCodeWriter.QUIET_ZONE_SIZE失败，原因："+e.getMessage());
+		}
+		BitMatrix matrix = write.encode(strContent,BarcodeFormat.QR_CODE,qrWH, qrWH,hints);
+//		BitMatrix matrix = new MultiFormatWriter().encode(strContent,BarcodeFormat.QR_CODE, qrWH, qrWH, hints);
+		int width = matrix.getWidth();
+		int height = matrix.getHeight();
+		
+		// 二维矩阵转为一维像素数组,也就是一直横着排了
+		int halfW = width / 2;
+		int halfH = height / 2;
+		int[] pixels = new int[width * height];
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				if (x > halfW - logoWH && x < halfW + logoWH && y > halfH - logoWH && y < halfH + logoWH) {
+					pixels[y * width + x] = mLogoBitmap.getPixel(x - halfW + logoWH, y - halfH + logoWH);
+				} else {
+					if (matrix.get(x, y)) {
+						pixels[y * width + x] = 0xff000000;
+					} else { 
+						//无信息设置像素点为白色
+						pixels[y * width + x] = 0xffffffff;
+					}
+				}
+			}
+		}
+		Bitmap bitmap = Bitmap.createBitmap(width, height,Bitmap.Config.ARGB_8888);
+		// 通过像素数组生成bitmap
+		bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+
 		return bitmap;
 	}
 	
