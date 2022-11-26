cb99556b498a68f8c9c88a43f15b82aeeafbd6eb
nanangarsyadw <haoxinanz413@163.com>
2022-11-26 19:34:11 +0800

    增加用户实体

---
+++ src/com/zftlive/android/auth/User.java
@@ -0,0 +1,18 @@
+package com.zftlive.android.auth;
+
+import com.zftlive.android.library.base.BaseEntity;
+
+/**
+ * 用户实体
+ * @author 曾繁添
+ * @version 1.0
+ *
+ */
+public class User extends BaseEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6421987575821946140L;
+
+}
